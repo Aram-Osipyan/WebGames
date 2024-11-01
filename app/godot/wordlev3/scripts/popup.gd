ec647782ee0b810d8ec87f4239d32db980363d56
Арам Осипян <aramosipan@192.168.0.12>
2024-11-01 23:44:29 +0300

    fix wordle resolution

--- app/godot/wordlev3/scripts/popup.gd
+++ app/godot/wordlev3/scripts/popup.gd
@@ -2,6 +2,7 @@ extends Panel
 
 var popup_tween
 var close_tween
+onready var height = OS.get_window_size().y
 
 func _ready():
 	popup_tween = Tween.new()
@@ -10,18 +11,16 @@ func _ready():
 	self.add_child(popup_tween)
 	self.add_child(close_tween)
 	
-	
-	
-	
-	
 	Global.connect("popup", self, "popup")
 	Global.connect("popup_close", self, "close")
+	
+	self.rect_position.y = height
 
 func popup():
 	popup_tween.remove_all()
 	popup_tween.interpolate_method(self, \
 		"set_position", \
-		Vector2(0,1024), \
+		Vector2(0, height), \
 		Vector2(0, 140), 0.4, 3)
 	popup_tween.interpolate_method(self, \
 		"set_position", \
@@ -35,7 +34,7 @@ func close():
 	close_tween.interpolate_method(self, \
 		"set_position", \
 		Vector2(0, 170), \
-		Vector2(0, 1024), 0.15, 1, 0, 0)
+		Vector2(0, height), 0.15, 1, 0, 0)
 	close_tween.start()
 
 func _on_info_button_pressed():
