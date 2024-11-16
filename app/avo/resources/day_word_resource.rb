efc63af1f99ed38d2c50ecad0ec1cacbc167449c
Арам Осипян <aramosipan@192.168.0.17>
2024-11-16 03:36:25 +0300

    fixes

--- app/avo/resources/day_word_resource.rb
+++ app/avo/resources/day_word_resource.rb
@@ -1,7 +1,7 @@
 class DayWordResource < Avo::BaseResource
   self.title  = :word
   self.includes = []
-  self.description = -> { "Текущее слово дня: #{DayWord.current_day_word.word.upcase}" }
+  self.description = -> { "Текущее слово дня: #{DayWord.current_day_word&.word&.upcase}" }
   self.search_query = -> do
     DayWord.where("word LIKE ?", "%#{params[:q]}%")
   end
