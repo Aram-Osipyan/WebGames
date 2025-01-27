22652eb78760bdf8e12cde315b616efc27ce3aa4
Арам Осипян <aramosipan@192.168.0.22>
2025-01-27 12:18:06 +0300

    fixes

--- app/controllers/wordle/prices_controller.rb
+++ app/controllers/wordle/prices_controller.rb
@@ -6,13 +6,13 @@ module Wordle
       result = ::Wordle::Stats::Show.perform(current_user)
 
       if result[:can_get_price]
-        # connection = Faraday.new(url: ' http://62.182.8.15:8800') do |conn|
-        #   conn.response :json
-        # end
+        connection = Faraday.new(url: ' http://62.182.8.15:8800') do |conn|
+          conn.response :json
+        end
   
-        # path = "/award_prize?msisdn=#{current_user.external_id}&prize=1"
+        path = "/award_prize?msisdn=#{current_user.external_id}&prize=1"
   
-        # response = connection.get(path)
+        response = connection.get(path)
 
         current_user.update!(last_rewarded_at: Time.current)
         result[:can_get_price] = false
