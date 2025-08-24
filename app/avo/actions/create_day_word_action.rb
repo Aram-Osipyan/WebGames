bc50901c9f556bd620da280e17f02f44964adcd6
Арам Осипян <aramosipan@MacBook-Air-von-Aram.local>
2025-08-25 01:33:59 +0300

    fixes default

--- app/avo/actions/create_day_word_action.rb
+++ app/avo/actions/create_day_word_action.rb
@@ -1,20 +1,19 @@
 class CreateDayWordAction < Avo::BaseAction
-  self.name = "Create Day Word Action"
+  self.name = 'Create Day Word Action'
   self.standalone = true
-  self.visible = -> do
+  self.visible = lambda {
     view == :index
-  end
+  }
 
   field :word, as: :text, required: true
-  field :active_until, as: :date_time, required: true, default: (DayWord.where('active_until is not null').order(:active_until).last.active_until + 1.day).end_of_day
+  field :active_until, as: :date_time, required: true
 
-  def handle(fields:, current_user:, resource:, **args)
-
+  def handle(fields:, current_user:, resource:, **_args)
     return error('Слово должно состоять из 5 букв') if fields[:word].length != 5
     return error('Такого слова не существует или этого слова нет в словаре') unless Word::Validate.valid?(fields[:word])
 
-    last_day_word = DayWord.order(:active_until).last
-    
+    DayWord.order(:active_until).last
+
     DayWord.create!(word: fields[:word], active_until: fields[:active_until])
 
     succeed 'Word created successfully'
