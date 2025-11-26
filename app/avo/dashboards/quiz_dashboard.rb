ee14c6939af386395c0285dcb6fe5b005bb4ffeb
Арам Осипян <aramosipan@MacBook-Air-von-Aram.local>
2025-11-26 17:58:17 +0300

    fixes

--- app/avo/dashboards/quiz_dashboard.rb
+++ app/avo/dashboards/quiz_dashboard.rb
@@ -7,6 +7,7 @@ require_relative 'cards/average_score_metric'
 
 class QuizDashboard < Avo::Dashboards::BaseDashboard
   self.id = 'quiz_dashboard'
+  self.name = 'Аналитика Викторины'
   self.description = 'Статистика и аналитика игр викторины'
   self.grid_cols = 3
 
@@ -24,11 +25,11 @@ class QuizDashboard < Avo::Dashboards::BaseDashboard
   divider label: 'Популярные категории'
 
   # Динамические карточки для категорий
-  QuizQuestion.joins(:quiz_user_answers)
-              .where('quiz_user_answers.created_at > ?', 7.days.ago)
-              .group(:category)
-              .count
-              .each do |category, count|
-    card category, number: count, description: 'ответов за неделю'
-  end
+  # QuizQuestion.joins(:quiz_user_answers)
+  #             .where('quiz_user_answers.created_at > ?', 7.days.ago)
+  #             .group(:category)
+  #             .count
+  #             .each do |category, count|
+  #   card category, number: count, description: 'ответов за неделю'
+  # end
 end
